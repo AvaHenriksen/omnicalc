ff9715bfc79f37d5b0b3d88754a49c68832354bd
AvaHenriksen <ava.henriksen@gmail.com>
2017-01-21 13:08:34 -0600

    Commit 2
    Finished loan and time between sections

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -41,7 +41,19 @@ class CalculationsController < ApplicationController
     # The principal value the user input is in the decimal @principal.
     # ================================================================================
 
-    @monthly_payment = "Replace this string with your answer."
+    @step_1 = @apr/(1200.0)
+
+    @step_2 = @years*12
+
+    @step_3 = @step_2*-1
+
+    @step_4 = (1+@step_1)**@step_3
+
+    @step_5 = @step_1/(1-@step_4)
+
+    @step_6 = @step_5*@principal
+
+    @monthly_payment = @step_6
 
     # ================================================================================
     # Your code goes above.
@@ -63,12 +75,12 @@ class CalculationsController < ApplicationController
     #   number of seconds as a result.
     # ================================================================================
 
-    @seconds = "Replace this string with your answer."
-    @minutes = "Replace this string with your answer."
-    @hours = "Replace this string with your answer."
-    @days = "Replace this string with your answer."
-    @weeks = "Replace this string with your answer."
-    @years = "Replace this string with your answer."
+    @seconds = @ending-@starting
+    @minutes = @seconds/60
+    @hours = @minutes/60
+    @days = @hours/24
+    @weeks = @days/7
+    @years = @days/365
 
     # ================================================================================
     # Your code goes above.
